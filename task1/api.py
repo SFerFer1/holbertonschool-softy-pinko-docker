5afbdbbe32a386256503f0814e86bd26a9bb87c3
SFerFer1 <10048@holbertonstudents.com>
2025-05-07 10:43:07 -0300

    task 0 and 1

---
+++ task1/api.py
@@ -0,0 +1,12 @@
+# it uses Flask to create one endpoint that returns “Hello, World!” when called
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/hello')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5252)
